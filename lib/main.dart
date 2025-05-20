2b3be50ee3f109105c93a131d92f03d6e703b97a
Vinícius Bornhofen <vini.Stormtrooper@gmail.com>
2025-05-20 11:45:52 -0300

    primeiro commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Container(
+    child: Text(
+      "Hello world flutter",
+      textDirection: TextDirection.ltr,
+      style: TextStyle(color: Colors.purpleAccent, fontSize: 30.0),
+
+      ),
+  ));
+}
